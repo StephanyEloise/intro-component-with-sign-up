ca3c4f8db338090d0f3049d71b206e60a53883b6
Stephany Eloise <stephany.eloise@hotmail.com>
2023-08-19 01:50:41 -0300

    Email

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
+document.getElementById("btn-submit").addEventListener("click", validateForm);
 
- document.getElementById("btn-submit").addEventListener("click", validateForm);
+let errorFirst = document.getElementById("error-first");
+let errorLast = document.getElementById("error-last");
+let errorEmail = document.getElementById("error-email");
+let errorPassword = document.getElementById("error-password");
 
+//function clearPlaceholder(input) {
+//    input.placeholder = "";
+//}
 
 function validateForm() {
     const firstName = document.getElementById("first-name");
     const lastName = document.getElementById("last-name");
     const email = document.getElementById("email");
     const password = document.getElementById("password");
-    const errorMessage = document.getElementById("errorMessage");
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    errorMessage.textContent = "";
+    errorFirst.textContent = "";
+    errorLast.textContent = "";
+    errorEmail.textContent = "";
+    errorPassword.textContent = "";
 
     if (firstName.value === "") {
         firstName.classList.add("error");
-        errorMessage.textContent = "First Name cannot be empty";
+        errorFirst.style.display = "block"; 
+        errorFirst.textContent = "First Name cannot be empty";
     } else {
         firstName.classList.remove("error");
+        errorFirst.style.display = "none"; 
     }
 
     if (lastName.value === "") {
         lastName.classList.add("error");
-        errorMessage.textContent = "Last Name cannot be empty";
+        errorLast.style.display = "block"; 
+        errorLast.textContent = "Last Name cannot be empty";
     } else {
         lastName.classList.remove("error");
+        errorLast.style.display = "none"; 
     }
-
-    if (email.value === "") {
+    
+    if (email.value === "" || !emailPattern.test(email.value)) {
         email.classList.add("error");
-        errorMessage.textContent = "Looks like this is not an email";
+        email.value = "email@example/com";
+        errorEmail.style.display = "block"; 
+        errorEmail.textContent = "Invalid email address";
     } else {
         email.classList.remove("error");
+        errorEmail.style.display = "none"; 
     }
 
     if (password.value === "") {
         password.classList.add("error");
-        errorMessage.textContent = "Password cannot be empty";
+        errorPassword.style.display = "block"; 
+        errorPassword.textContent = "Password cannot be empty";
     } else {
-        message.classList.remove("error");
+        password.classList.remove("error");
+        errorPassword.style.display = "none"; 
     }
 }
